f7507ee61735e7984d20ea5bfa85748404afce78
Pisut Theeraphanphichate <pisut.flyinggiraffe@gmail.com>
2026-02-19 10:53:29 +0700

    add create and update movie dto

---
+++ src/movie/dtos/create-movie.dto.ts
@@ -0,0 +1,13 @@
+export class CreateMovieDto {
+  title: string;
+
+  releaseYear?: number;
+
+  contentRating?: any;
+
+  length?: number;
+
+  genres?: any;
+
+  outline?: string;
+}
---
+++ src/movie/dtos/update-movie.dto.ts
@@ -0,0 +1,13 @@
+export class UpdateMovieDto {
+  title?: string;
+
+  releaseYear?: number;
+
+  contentRating?: any;
+
+  length?: number;
+
+  genres?: any;
+
+  outline?: string;
+}
